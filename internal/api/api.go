a7d036aa0b93e7d3bac112c867ec955292fac46f
Oyetomi <oyetomiabiodun@yahoo.com>
2022-04-30 19:14:22 +0100

    API: added SetSettings() function

--- internal/api/api.go
+++ internal/api/api.go
@@ -23,10 +23,11 @@ func GetApiVersion() string {
 }
 
 func Login(username, password string) string {
-	resp, err := client.R().SetFormData(map[string]string{
-		"username": username,
-		"password": password,
-	}).Post("/auth/login")
+	resp, err := client.R().SetFormData(
+		map[string]string{
+			"username": username,
+			"password": password,
+		}).Post("/auth/login")
 	if err != nil {
 		log.Println(errors.ErrLoginFailed)
 	}
@@ -46,6 +47,18 @@ func GetSettings(sessionid string) string {
 	}
 	if resp.StatusCode() != 200 {
 		log.Println(resp.String())
+	}
+	return resp.String()
+}
+
+func SetSettings(settings, sessionid string) string {
+	resp, err := client.R().SetFormData(
+		map[string]string{
+			"settings":  settings,
+			"sessionid": sessionid,
+		}).Post("/auth/settings/set")
+	if err != nil {
+		log.Println(errors.ErrCouldNotSetSettings)
 	}
 	return resp.String()
 }
