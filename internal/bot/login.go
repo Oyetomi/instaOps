8442e4dafc90e0af25eae983af3fd08b7ae5ef92
Oyetomi <oyetomiabiodun@yahoo.com>
2022-05-03 20:36:00 +0100

    refactored Login()

--- internal/bot/login.go
+++ internal/bot/login.go
@@ -1,84 +1,30 @@
 package bot
 
 import (
-	"errors"
 	"github.com/Oyetomi/instaOps/internal/api"
+	"github.com/Oyetomi/instaOps/internal/file"
 	"github.com/sirupsen/logrus"
-	"os"
-	"path/filepath"
 )
 
 const (
 	settingsPath = "../config/settings.json"
 )
 
-func createAbsolutePath(FilePath string) string {
-	AbsFilePath, err := filepath.Abs(FilePath)
-	if err != nil {
-		logrus.Fatal(err)
-	}
-	return AbsFilePath
-}
-
-func createFile(FilePath string) {
-	if _, err := os.Create(FilePath); err != nil {
-		logrus.Fatalf("could not create %v", FilePath)
-	} else {
-		logrus.Infof("%v file created", FilePath)
-	}
-}
-
-func checkIfFileExists(FilePath string) {
-	if info, err := os.Stat(FilePath); err == nil {
-		logrus.Infof("%v file exists", info.Name())
-	} else {
-		if errors.Is(err, os.ErrNotExist) {
-			createFile(FilePath)
-		}
-	}
-}
-
-func writeToFile(Filepath string, contents []byte) {
-	if err := os.WriteFile(Filepath, contents, 0644); err != nil {
-		logrus.Fatalf("could not write contents to %v", Filepath)
-	}
-	logrus.Infof("wrote contents to %v ", Filepath)
-}
-
-func readFileContents(Filepath string) string {
-	contents, err := os.ReadFile(Filepath)
-	if err != nil {
-		logrus.Infof("could not read contents of %v", Filepath)
-	}
-	return string(contents)
-}
-
-func isEmptyFile(FilePath string) bool {
-	if info, err := os.Stat(FilePath); err == nil {
-		if info.Size() == 0 {
-			logrus.Warnf("%v is empty", FilePath)
-		} else {
-			return false
-		}
-	}
-	return true
-}
-
 func Login(username, password string) (sessionid string) {
 	logrus.Println("logging in...")
 	// get absolute path to config/settings.json
-	settingsPath := createAbsolutePath(settingsPath)
-	checkIfFileExists(settingsPath)
+	settingsPath := file.CreateAbsolutePath(settingsPath)
+	file.CheckIfFileExists(settingsPath)
 	// check if file is empty
-	if isEmptyFile(settingsPath) {
+	if file.IsEmptyFile(settingsPath) {
 		// if file is empty, we do a manual login
 		sessionid = api.Login(username, password)
 		// get settings.json and save it into settings.json config file
 		settings := api.GetSettings(sessionid)
 		logrus.Infof("writing cookie to %v... ", settingsPath)
-		writeToFile(settingsPath, []byte(settings))
+		file.WriteToFile(settingsPath, []byte(settings))
 	} else {
-		contents := readFileContents(settingsPath)
+		contents := file.ReadFileContents(settingsPath)
 		// retrieve sessionid so we can log in
 		sessionid = api.SetSettings(string(contents))
 		logrus.Infof("sessionID from %v -> %v", settingsPath, sessionid)
