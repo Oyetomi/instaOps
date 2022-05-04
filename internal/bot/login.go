ff724577223ca3cbf4a794a4b794cf758c23e07c
Oyetomi <oyetomiabiodun@yahoo.com>
2022-05-04 14:46:19 +0100

    refactored Login function

--- internal/bot/login.go
+++ internal/bot/login.go
@@ -7,24 +7,39 @@ import (
 )
 
 const (
-	settingsPath = "../config/settings.json"
+	settingsPath = "./config/settings.json"
 )
 
 func Login(username, password string) (sessionid string) {
 	logrus.Println("logging in...")
 	// get absolute path to config/settings.json
-	settingsPath := file.CreateAbsolutePath(settingsPath)
-	file.CheckIfFileExists(settingsPath)
+	settingsPath, err := file.CreateAbsolutePath(settingsPath)
+	if err != nil {
+		logrus.Errorf("could not create %v", settingsPath)
+	}
+	info, err := file.CheckIfFileExists(settingsPath)
+	if err != nil {
+		logrus.Errorf("%v does not exist", info.Name())
+	}
 	// check if file is empty
-	if file.IsEmptyFile(settingsPath) {
+	empty := file.IsEmptyFile(settingsPath)
+	if empty {
 		// if file is empty, we do a manual login
 		sessionid = api.Login(username, password)
 		// get settings.json and save it into settings.json config file
 		settings := api.GetSettings(sessionid)
-		logrus.Infof("writing cookie to %v... ", settingsPath)
-		file.WriteToFile(settingsPath, []byte(settings))
+		logrus.Infof("writing cookie to %v ", settingsPath)
+		// write cookies to file
+		err := file.WriteToFile(settingsPath, []byte(settings))
+		if err != nil {
+			logrus.Fatalf("could not write cookie to %v", settingsPath)
+		}
 	} else {
-		contents := file.ReadFileContents(settingsPath)
+		// read cookies
+		contents, err := file.ReadFileContents(settingsPath)
+		if err != nil {
+			logrus.Fatalf("could not read cookies from %v", settingsPath)
+		}
 		// retrieve sessionid so we can log in
 		sessionid = api.SetSettings(string(contents))
 		logrus.Infof("sessionID from %v -> %v", settingsPath, sessionid)
