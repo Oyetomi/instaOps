e5c2198953c83e4c9f9d1ae2124261484f3896b4
Oyetomi <oyetomiabiodun@yahoo.com>
2022-05-06 21:21:06 +0100

    alias logrus to log

--- internal/bot/login.go
+++ internal/bot/login.go
@@ -3,27 +3,27 @@ package bot
 import (
 	"github.com/Oyetomi/instaOps/internal/api"
 	"github.com/Oyetomi/instaOps/internal/file"
-	"github.com/sirupsen/logrus"
+	log "github.com/sirupsen/logrus"
 )
 
 const (
-	settingsPath = "..cmd/settings/settings.json"
+	settingsPath = "../settings/settings.json"
 )
 
 func Login(username, password string) (sessionid string) {
-	logrus.Println("logging in...")
+	log.Println("logging in...")
 
 	// get absolute path to settings/settings.json
 	settingsPath, err := file.CreateAbsolutePath(settingsPath)
 	if err != nil {
-		logrus.Errorf("could not create %v", settingsPath)
+		log.Errorf("could not create %v", settingsPath)
 	}
 
 	info, err := file.CheckIfFileExists(settingsPath)
 	if err != nil {
-		logrus.Errorf("%v does not exist", info.Name())
+		log.Errorf("%v does not exist", info.Name())
 	}
-	
+
 	// check if file is empty
 	empty := file.IsEmptyFile(settingsPath)
 	if empty {
@@ -31,21 +31,21 @@ func Login(username, password string) (sessionid string) {
 		sessionid = api.Login(username, password)
 		// get settings.json and save it into settings.json settings file
 		settings := api.GetSettings(sessionid)
-		logrus.Infof("writing cookie to %v ", settingsPath)
+		log.Infof("writing cookie to %v ", settingsPath)
 		// write cookies to file
 		err := file.WriteToFile(settingsPath, []byte(settings))
 		if err != nil {
-			logrus.Fatalf("could not write cookie to %v", settingsPath)
+			log.Fatalf("could not write cookie to %v", settingsPath)
 		}
 	} else {
 		// read cookies
 		contents, err := file.ReadFileContents(settingsPath)
 		if err != nil {
-			logrus.Fatalf("could not read cookies from %v", settingsPath)
+			log.Fatalf("could not read cookies from %v", settingsPath)
 		}
 		// retrieve sessionid so we can log in
 		sessionid = api.SetSettings(string(contents))
-		logrus.Infof("sessionID from %v -> %v", settingsPath, sessionid)
+		log.Infof("sessionID from %v -> %v", settingsPath, sessionid)
 	}
 	return sessionid
 }
