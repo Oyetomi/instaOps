4a18f3191bf8a998ab2212b7aa51ca63ea93fb1d
Oyetomi <oyetomiabiodun@yahoo.com>
2022-05-29 10:00:10 +0100

    added comments

--- internal/bot/login_test.go
+++ internal/bot/login_test.go
@@ -1,3 +1,6 @@
+/*
+login_test.go compares the length of the sessionID returned by the Login function
+*/
 package bot
 
 import (
@@ -9,10 +12,12 @@ func TestLogin(t *testing.T) {
 		sessionid1 string
 		sessionid2 string
 	}{
+		// random sessionID with length 34 or 35
 		{"12345678912%1234567812%90876543212", "12345678912%1234567812%908765432123"},
 	}
 	got := Login("username", "password")
 	for _, tc := range testCases {
+		// comparing length of sessionID with the length of sessionID returned by Login function
 		if len(got) != len(tc.sessionid1) && len(got) != len(tc.sessionid2) {
 			t.Errorf("TestLogin() returned %v; wanted %v or %v", len(got), len(tc.sessionid1), len(tc.sessionid2))
 		}
