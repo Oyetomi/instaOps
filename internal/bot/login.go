b36bae07742dd01c1346ea22804cef1c0c324d69
Oyetomi <oyetomiabiodun@yahoo.com>
2022-06-25 17:01:34 +0100

    optional login paramters from yaml file

--- internal/bot/login.go
+++ internal/bot/login.go
@@ -3,49 +3,58 @@ package bot
 import (
 	"github.com/Oyetomi/instaOps/internal/api"
 	"github.com/Oyetomi/instaOps/internal/file"
-	log "github.com/sirupsen/logrus"
+	"log"
 )
 
 const (
-	settingsPath = "../settings/settings.json"
+	settingsPath = "../cmd/settings/settings.json"
+	yamlPath     = "../cmd/settings/config.yaml"
 )
 
-func Login(username, password string) (sessionid string) {
-	log.Println("logging in...")
+type Config struct {
+	Username          string `yaml:"username"`
+	Password          string `yaml:"password"`
+	Verification_code string `yaml:"verification_code"`
+	Proxy             string `yaml:"proxy"`
+	Locale            string `yaml:"locale"`
+	Timezone          string `yaml:"timezone"`
+	SettingsPath      string `yaml:"settingsPath"`
+	YamlPath          string `yaml:"yamlPath"`
+}
 
-	// get absolute path to settings/settings.json
-	settingsPath, err := file.CreateAbsolutePath(settingsPath)
+func Login() (sessionid string) {
+	var c Config
+	cs := file.ReadConfig(c, yamlPath)
+	log.Printf("Logging into %s", cs.Username)
+	absPath, err := file.CreateAbsolutePath(settingsPath)
 	if err != nil {
-		log.Errorf("could not create %v", settingsPath)
+		log.Fatalf("Could not create %v", absPath)
 	}
-
-	info, err := file.CheckIfFileExists(settingsPath)
+	info, err := file.CheckIfFileExists(absPath)
 	if err != nil {
-		log.Errorf("%v does not exist", info.Name())
+		log.Fatalf("%v does not exist", info.Name())
 	}
-
-	// check if file is empty
-	empty := file.IsEmptyFile(settingsPath)
+	empty := file.IsEmptyFile(absPath)
 	if empty {
 		// if file is empty, we do a manual login
-		sessionid = api.Login(username, password)
+		sessionid := api.Login(cs.Username, cs.Password, cs.Verification_code, cs.Proxy, cs.Locale, cs.Timezone)
 		// get settings.json and save it into settings.json settings file
 		settings := api.GetSettings(sessionid)
-		log.Infof("writing cookie to %v ", settingsPath)
+		log.Printf("Wrote cookie to %v ", absPath)
 		// write cookies to file
-		err := file.WriteToFile(settingsPath, []byte(settings))
+		err := file.WriteToFile(absPath, []byte(settings))
 		if err != nil {
-			log.Fatalf("could not write cookie to %v", settingsPath)
+			log.Fatalf("Could not write cookie to %v", absPath)
 		}
 	} else {
 		// read cookies
-		contents, err := file.ReadFileContents(settingsPath)
+		contents, err := file.ReadFileContents(absPath)
 		if err != nil {
-			log.Fatalf("could not read cookies from %v", settingsPath)
+			log.Fatalf("Could not read cookies from %v", absPath)
 		}
 		// retrieve sessionid so we can log in
 		sessionid = api.SetSettings(string(contents))
-		log.Infof("sessionID from %v -> %v", settingsPath, sessionid)
+		log.Printf("Successfully logged in to %s", cs.Username)
 	}
 	return sessionid
 }
