8340271089b1e90bce1ebf0d2027e9d5692847d0
Oyetomi <oyetomiabiodun@yahoo.com>
2022-07-04 08:18:45 +0100

    refactoring

--- internal/bot/media/likers.go
+++ internal/bot/media/likers.go
@@ -4,24 +4,25 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/Oyetomi/instaOps/internal/api"
+	"github.com/sirupsen/logrus"
 	"io"
-	"log"
 )
 
 type Media struct {
 	Pk string `json:"pk"`
 }
 
-// GetMediaLikers returns 100 media likers of a particular media
+// GetMediaLikers returns 1000 media likers of a particular media
 func GetMediaLikers(sessionid, media_id string, writer io.Writer) {
 	var m []Media
 	likers := api.GetMediaLikers(sessionid, media_id)
 	if err := json.Unmarshal([]byte(likers), &m); err != nil {
-		log.Fatal(err)
+		logrus.Fatal(err)
 	}
 	for _, v := range m {
 		if _, err := fmt.Fprintln(writer, v.Pk); err != nil {
-			log.Fatalf("Couldn't write to %v", writer)
+			logrus.Fatalf("Couldn't write to %v", writer)
 		}
 	}
+	logrus.Println("info written successfully")
 }
